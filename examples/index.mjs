3bdca1423a623035b1b2805abefebb9760e3f24d
Joey Guerra <jguerra@thoughtworks.com>
2019-08-19 18:25:10 -0500

    read a file

--- examples/index.mjs
+++ examples/index.mjs
@@ -1,5 +1,7 @@
 import tap from "tap"
-tap.test("Just start", t => {
-    t.fail()
+const MONTHS = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+
+tap.test("Find month name", t => {
+    t.ok(MONTHS[0] == "JAN")
     t.end()
 })
--- index.mjs
+++ index.mjs
@@ -1,9 +1,10 @@
 import GoogleApis from "googleapis"
 import fs from "fs"
 import readline from "readline"
+import Dates from "./lib/Dates.mjs"
 
 const File = fs.promises
-const SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+const SCOPES = ["https://www.googleapis.com/auth/drive"]
 const google = GoogleApis.google
 const TOKEN_PATH = "token.json"
 
@@ -15,7 +16,7 @@ const GoogleDrive = {
     },
     async get(authClient, options){
         const drive = google.drive({version: "v3", auth: authClient})
-        const response = await drive.files.get(options)
+        const response = await drive.files.export(options)
         return response
     },
     async isAuthed(){
@@ -57,11 +58,46 @@ async function main(){
     let token = await GoogleDrive.isAuthed()
     if(token != null) oAuth2Client.setCredentials(JSON.parse(token))
     else await GoogleDrive.executeAuthSequence(oAuth2Client)
-    let response = await GoogleDrive.list(oAuth2Client, {q: "name = 'May 1'",
+    let response = await GoogleDrive.list(oAuth2Client, {q: "name = 'DAILY ANNOUNCEMENTS'",
         corpora: "user",
+        fields: "nextPageToken, files(id, name),files/parents",
         pageToken: null
     })
-    console.log(response)
+    let id = response.data.files[0].id
+    let today = new Date()
+    let folderName = Dates.MONTHS[today.getMonth()]
+    response = await GoogleDrive.list(oAuth2Client, {q: `'${id}' in parents`,
+        corpora: "user",
+        fields: "nextPageToken, files(id, name),files/parents",
+        pageToken: null
+    })
+    response = await GoogleDrive.list(oAuth2Client, {q: `'${response.data.files.find(f=>f.name==folderName).id}' in parents`,
+        corpora: "user",
+        fields: "nextPageToken, files(id, name),files/parents",
+        pageToken: null
+    })
+
+    let days = Dates.weekMondayThurFridayRange(today).map(d => {
+        let month = Dates.formatMonthDate(d).toLowerCase()
+        month = `${month.charAt(0).toUpperCase()}${month.slice(1)}`
+        return month
+    })
+    let thisWeeksFiles = []
+    for(let i = 0; i < days.length-1; i++) {
+        let day = days[i]
+        let filesForDay = await GoogleDrive.list(oAuth2Client, {q: `'${response.data.files.find(f=>f.name == day).id}' in parents`,
+            corpora: "user",
+            fields: "nextPageToken, files(id, name),files/parents",
+            pageToken: null
+        })
+        filesForDay.data.files.forEach(f=>thisWeeksFiles.push(f))
+    }
+    let fileMeta = await GoogleDrive.get(oAuth2Client, {
+        fileId: thisWeeksFiles[0].id,
+        mimeType: "text/plain"
+    })
+    //console.log(thisWeeksFiles)
+    console.log(fileMeta.data)
 }
 
 main().then(c=>{}).catch(e=>console.error(e))
