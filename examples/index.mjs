2017d1ae629c29c17f7fc31928a8647801f5a906
Joey Guerra <jguerra@thoughtworks.com>
2019-08-22 16:35:21 -0500

    remove worthless test

--- examples/index.mjs
+++
@@ -1,7 +0,0 @@
-import tap from "tap"
-const MONTHS = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
-
-tap.test("Find month name", t => {
-    t.ok(MONTHS[0] == "JAN")
-    t.end()
-})
